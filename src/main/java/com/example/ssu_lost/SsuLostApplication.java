ee3d600a5381004b977170a2d669d4022607aa06
leedy5521 <leedy5521@naver.com>
2025-07-08 17:18:35 +0900

    첫 커밋

---
+++ src/main/java/com/example/ssu_lost/SsuLostApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssu_lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuLostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuLostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssu_lost/SsuLostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssu_lost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuLostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
