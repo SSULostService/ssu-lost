c5d153e4fc5e40ccaf0d0e74c6ad5d367d855915
leedy5521 <leedy5521@naver.com>
2025-07-22 02:14:14 +0900

    [FEAT]구글 인가코드 받아오기

---
+++ src/main/java/com/example/ssu_lost/oauth/google/controller/GoogleLoginController.java
@@ -0,0 +1,25 @@
+package com.example.ssu_lost.oauth.google.controller;
+
+import com.example.ssu_lost.global.code.ResponseCode;
+import com.example.ssu_lost.global.response.ApiResponse;
+import com.example.ssu_lost.oauth.kakao.dto.KakaoUserInfoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class GoogleLoginController {
+
+    @GetMapping("/callback/google")
+    public ApiResponse<?> callback(@RequestParam("code") String code) {
+
+        return ApiResponse.onSuccess(ResponseCode.OK, code);
+    }
+}
---
+++ src/main/java/com/example/ssu_lost/oauth/kakao/controller/LoginPageController.java
@@ -0,0 +1,38 @@
+package com.example.ssu_lost.oauth.kakao.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginPageController {
+// kakao
+    @Value("${spring.kakao.kakao_client_id}")
+    private String kakaoClientId;
+
+    @Value("${spring.kakao.kakao_redirect_uri}")
+    private String kakaoRedirectUri;
+//google
+    @Value("${spring.google.google_client_id}")
+    private String googleClientId;
+
+    @Value("${spring.google.google_redirect_uri}")
+    private String googleRedirectUri;
+
+
+    @GetMapping("/page")
+    public String loginPage(Model model) {
+        String kakaoLocation = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+kakaoClientId+"&redirect_uri="+kakaoRedirectUri;
+
+        String googleLocation = "https://accounts.google.com/o/oauth2/v2/auth?client_id="+googleClientId+"&redirect_uri="+googleRedirectUri+"&response_type=code&scope=email profile";
+
+        model.addAttribute("kakaoLocation", kakaoLocation);
+        model.addAttribute("googleLocation", googleLocation);
+
+        return "login";
+    }
+
+}
