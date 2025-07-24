a71fda0fd1bd9446f226cba52d0848708d87ae6d
leedy5521 <leedy5521@naver.com>
2025-07-24 15:29:35 +0900

    [FEAT]회원 조회 메서드 추가

--- src/main/java/com/example/ssu_lost/repository/MemberRepository.java
+++ src/main/java/com/example/ssu_lost/repository/MemberRepository.java
@@ -1,4 +1,13 @@
 package com.example.ssu_lost.repository;
 
-public interface MemberRepository {
+import com.example.ssu_lost.entity.Member;
+import com.example.ssu_lost.enums.OAuthProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+    Optional<Member> findByOAuthProviderAndOAuthId(OAuthProvider oAuthProvider, String oAuthId);
 }
