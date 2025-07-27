e3afd76e9e7e32f103c76700a9756f894373aa6f
chounjae <blueskycuj@naver.com>
2025-07-27 19:45:30 +0900

    [FEAT] 분실물 홈 조회 JPA 작성

--- src/main/java/com/example/ssu_lost/repository/LostItemRepository.java
+++ src/main/java/com/example/ssu_lost/repository/LostItemRepository.java
@@ -1,4 +1,11 @@
 package com.example.ssu_lost.repository;
 
-public interface LostItemRepository {
+import com.example.ssu_lost.entity.LostItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LostItemRepository extends JpaRepository<LostItem,Long> {
+    //TO DO: 홈 조회 기능 확정 시 변경
+    List<LostItem> findTop9ByOrderByCreatedDateDesc();
 }
