85ef587c422c442bb5b5ba2581ff68e9326a80dd
leedy5521 <leedy5521@naver.com>
2025-07-29 01:31:05 +0900

    [FEAT]LoginService 생성

--- src/main/java/com/example/ssu_lost/oauth/login/service/LoginService.java
+++ src/main/java/com/example/ssu_lost/oauth/login/service/LoginService.java
@@ -1,21 +1,48 @@
 package com.example.ssu_lost.oauth.login.service;
 
+import com.example.ssu_lost.entity.Member;
 import com.example.ssu_lost.enums.OAuthProvider;
+import com.example.ssu_lost.global.code.ErrorCode;
+import com.example.ssu_lost.global.exception.LoginException;
+import com.example.ssu_lost.oauth.OAuthUserInfo;
 import com.example.ssu_lost.oauth.google.service.GoogleService;
 import com.example.ssu_lost.oauth.kakao.service.KakaoService;
 import com.example.ssu_lost.repository.MemberRepository;
+import com.example.ssu_lost.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class LoginService {
 
     private final MemberRepository memberRepository;
     private final KakaoService kakaoService;
     private final GoogleService googleService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public String socialLogin(OAuthProvider oAuthProvider) {
-        return "";
+    public String socialLogin(OAuthProvider oAuthProvider, String code) {
+        OAuthUserInfo userInfo = switch (oAuthProvider) {
+            case GOOGLE -> googleService.getUserInfo(code);
+            case KAKAO -> kakaoService.getUserInfo(code);
+            default -> throw new LoginException(ErrorCode._LOGIN_BAD_REQUEST);
+        };
+
+        Member member = memberRepository.findBySocialLoginInfo(oAuthProvider, userInfo.getProviderId())
+                .orElseGet(() -> {
+                    Member newMember = Member.builder()
+                            .id(UUID.randomUUID())
+                            .memberName(userInfo.getName())
+                            .oAuthProvider(oAuthProvider)
+                            .oAuthId(userInfo.getProviderId())
+                            .build();
+                    return memberRepository.save(newMember);
+                });
+
+        return jwtTokenProvider.createAccessToken(member.getId().toString());
     }
 }
