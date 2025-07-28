2f668fded7e037ce3e719f7545419d85b41f8eee
leedy5521 <leedy5521@naver.com>
2025-07-28 15:19:04 +0900

    [FIX]callback 메서드 수정

--- src/main/java/com/example/ssu_lost/oauth/login/controller/LoginController.java
+++ src/main/java/com/example/ssu_lost/oauth/login/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.ssu_lost.oauth.login.controller;
 
+import com.example.ssu_lost.enums.OAuthProvider;
 import com.example.ssu_lost.global.code.ResponseCode;
 import com.example.ssu_lost.global.response.ApiResponse;
 import com.example.ssu_lost.oauth.OAuthUserInfo;
@@ -7,6 +8,7 @@ import com.example.ssu_lost.oauth.google.dto.GoogleUserInfoDto;
 import com.example.ssu_lost.oauth.google.service.GoogleService;
 import com.example.ssu_lost.oauth.kakao.dto.KakaoUserInfoDto;
 import com.example.ssu_lost.oauth.kakao.service.KakaoService;
+import com.example.ssu_lost.oauth.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,18 +24,17 @@ public class LoginController {
 
     private final KakaoService kakaoService;
     private final GoogleService googleService;
+    private final LoginService loginService;
 
     @GetMapping("/callback/kakao")
-    public ApiResponse<KakaoUserInfoDto> callbackKakao(@RequestParam("code") String code) {
-        KakaoUserInfoDto kakaoUserInfoDto = kakaoService.getUserInfo(code);
+    public ApiResponse<String> callbackKakao(@RequestParam("code") String code) {
 
-        return ApiResponse.onSuccess(ResponseCode.OK, kakaoUserInfoDto);
+        return ApiResponse.onSuccess(ResponseCode.OK, loginService.socialLogin(OAuthProvider.KAKAO, code));
     }
 
     @GetMapping("/callback/google")
-    public ApiResponse<OAuthUserInfo> callbackGoogle(@RequestParam("code") String code) {
-        OAuthUserInfo oAuthUserInfo = googleService.getUserInfo(code);
+    public ApiResponse<String> callbackGoogle(@RequestParam("code") String code) {
 
-        return ApiResponse.onSuccess(ResponseCode.OK, oAuthUserInfo);
+        return ApiResponse.onSuccess(ResponseCode.OK, loginService.socialLogin(OAuthProvider.GOOGLE, code));
     }
 }
