e4c32692ce5f870cfff051ce3763067882b08588
leedy5521 <leedy5521@naver.com>
2025-07-29 01:30:07 +0900

    [FIX]findBySocialLoginInfo로 변경

--- src/main/java/com/example/ssu_lost/repository/MemberRepository.java
+++ src/main/java/com/example/ssu_lost/repository/MemberRepository.java
@@ -3,12 +3,15 @@ package com.example.ssu_lost.repository;
 import com.example.ssu_lost.entity.Member;
 import com.example.ssu_lost.enums.OAuthProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
 
-    Optional<Member> findByoAuthProviderAndOAuthId(OAuthProvider oAuthProvider, String oAuthId);
+    @Query("SELECT m FROM Member m WHERE m.oAuthProvider = :oAuthProvider AND m.oAuthId = :oAuthId")
+    Optional<Member> findBySocialLoginInfo(@Param("oAuthProvider") OAuthProvider oAuthProvider, @Param("oAuthId") String oAuthId);
 
 }
