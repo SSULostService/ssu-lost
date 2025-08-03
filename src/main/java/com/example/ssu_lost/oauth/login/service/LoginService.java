38439e016fa239b8c15cb9f5a62ff1f069d0d6fa
leedy5521 <leedy5521@naver.com>
2025-08-03 16:08:39 +0900

    LoginService oAuthprovider OCP만족 위해 수정

--- src/main/java/com/example/ssu_lost/oauth/login/service/LoginService.java
+++ src/main/java/com/example/ssu_lost/oauth/login/service/LoginService.java
@@ -5,32 +5,36 @@ import com.example.ssu_lost.enums.OAuthProvider;
 import com.example.ssu_lost.global.code.ErrorCode;
 import com.example.ssu_lost.global.exception.LoginException;
 import com.example.ssu_lost.oauth.OAuthUserInfo;
-import com.example.ssu_lost.oauth.google.service.GoogleService;
-import com.example.ssu_lost.oauth.kakao.service.KakaoService;
+import com.example.ssu_lost.oauth.SocialLoginService;
 import com.example.ssu_lost.repository.MemberRepository;
 import com.example.ssu_lost.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
 @Transactional
 public class LoginService {
 
     private final MemberRepository memberRepository;
-    private final KakaoService kakaoService;
-    private final GoogleService googleService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final Map<OAuthProvider, SocialLoginService> socialLoginServices;
+
+    public LoginService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider, List<SocialLoginService> services) {
+        this.memberRepository = memberRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.socialLoginServices = services.stream()
+                .collect(Collectors.toMap(SocialLoginService::getProviderName, Function.identity()));
+    }
 
     public String socialLogin(OAuthProvider oAuthProvider, String code) {
-        OAuthUserInfo userInfo = switch (oAuthProvider) {
-            case GOOGLE -> googleService.getUserInfo(code);
-            case KAKAO -> kakaoService.getUserInfo(code);
-            default -> throw new LoginException(ErrorCode._LOGIN_BAD_REQUEST);
-        };
+        SocialLoginService socialLoginService = socialLoginServices.get(oAuthProvider);
+        OAuthUserInfo userInfo = socialLoginService.getUserInfo(code);
 
         Member member = memberRepository.findBySocialLoginInfo(oAuthProvider, userInfo.getProviderId())
                 .orElseGet(() -> memberRepository.save(Member.from(userInfo)));
