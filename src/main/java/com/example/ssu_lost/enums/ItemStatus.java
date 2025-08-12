bf2649c40da114188cd10d06615da8452d6c1b27
chounjae <blueskycuj@naver.com>
2025-08-12 18:59:29 +0900

    [FEAT] String 값으로 Status 값 받기

--- src/main/java/com/example/ssu_lost/enums/ItemStatus.java
+++ src/main/java/com/example/ssu_lost/enums/ItemStatus.java
@@ -1,5 +1,9 @@
 package com.example.ssu_lost.enums;
 
 public enum ItemStatus {
-    FINDING, KEEPING, COMPLETE
+    FINDING, KEEPING, COMPLETE;
+
+    public static ItemStatus fromString(String status) {
+        return ItemStatus.valueOf(status.toUpperCase());
+    }
 }
