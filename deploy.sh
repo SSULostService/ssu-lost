2b50e3cf39f9d8c624614087c29a5743124c21d2
chounjae <blueskycuj@naver.com>
2025-08-25 15:55:12 +0900

    [FEAT] 헬스 체크 변경

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ docker pull $DOCKER_USERNAME/$APP_NAME:latest
 # 2. idle 컨테이너 실행 (새 이미지)
 docker compose up -d $IDLE
 
-# 3. idle 컨테이너 헬스체크 대기
+# 3. idle 컨테이너 헬스체크
 echo "Waiting for $IDLE-container health..."
 timeout=120
 count=0
@@ -34,15 +34,6 @@ until [ "$(docker inspect -f '{{.State.Health.Status}}' ${IDLE}-container)" = "h
   sleep 5
   count=$((count+5))
 done
-
-if [ $count -ge $timeout ]; then
-  echo "헬스체크 실패 ❌"
-  # 실패 시 새 컨테이너 정리, 기존 서비스 유지(롤백)
-  docker stop ${IDLE}-container
-  docker rm ${IDLE}-container
-  echo "롤백 실행: 이전 서비스 유지 ⏪"
-  exit 1
-fi
 echo "$IDLE-container is healthy ✅"
 
 # 4. Nginx 트래픽 전환
